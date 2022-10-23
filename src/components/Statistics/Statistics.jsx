a31dfcb708ef403cae6b73922af5c5feac0cd1ff
kostya <kostizavali@gmail.com>
2022-10-23 13:49:23 +0300

    task2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,27 +1,28 @@
 import PropTypes from 'prop-types';
+import css from '../Statistics/Statistics.module.css';
 
-export const Statistics = ({title,stats}) => {
-    return (
-        <section className="statistics">
-  {title && <h2 className='title'>{title}</h2>}
-  <ul className="stat-list">
-    {stats.map(({ id, label, percentage }) => (
-      <li key ={id} className="item">
-      <span className="label">{label}</span>
-      <span className="percentage">{percentage}</span>
-    </li>
-    ))}   
-  </ul>
-</section>
-    );
-  };
-  Statistics.protoType = {
-    title: PropTypes.string,
-    stats: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        label: PropTypes.string.isRequired,
-        percentage: PropTypes.number.isRequired,
-      })
-    ),
-  };
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.section}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.list}>
+        {stats.map(({ id, label, percentage }) => (
+          <li key={id} className={css.item}>
+            <span className="label">{label}</span>
+            <span className="percentage">{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+Statistics.protoType = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};

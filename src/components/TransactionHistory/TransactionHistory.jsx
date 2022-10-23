8df1e1668c9283d6b17fa23fd2122687f10dacbe
kostya <kostizavali@gmail.com>
2022-10-23 16:07:58 +0300

    end

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,22 +1,22 @@
 import PropTypes from 'prop-types';
-
+import css from '../TransactionHistory/TransactionHistory.module.css';
 export const TransactionHistory = ({ items }) => {
   return (
-    <table className="transaction-history">
-      <thead>
+    <table className={css.transaction}>
+      <thead className={css.thead}>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={css.th}>Type</th>
+          <th className={css.th}>Amount</th>
+          <th className={css.th}>Currency</th>
         </tr>
       </thead>
       <tbody>
         {items.map(({ id, type, amount, currency }) => {
           return (
-            <tr key={id}>
-              <td>{type}</td>
-              <td>{amount}</td>
-              <td>{currency}</td>
+            <tr key={id} className={css.treee}>
+              <td className={css.td}>{type}</td>
+              <td className={css.td}>{amount}</td>
+              <td className={css.td}>{currency}</td>
             </tr>
           );
         })}

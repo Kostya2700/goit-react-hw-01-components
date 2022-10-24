8b6290bc95100fd1a737822a18a1d5a714a6198b
kostya <kostizavali@gmail.com>
2022-10-24 20:23:14 +0300

    mistakes

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,20 +1,18 @@
 import PropTypes from 'prop-types';
-import { ListItem } from '../FriendListItem/FriendList.style';
-import css from './FriendList.module.css';
+import { ListItem } from './FriendList.style';
+
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
 export const FriendList = ({ friends }) => {
   return (
     <ListItem>
       {friends.map(({ avatar, name, isOnline, id }) => (
-        <li key={id} className={css.item}>
-          {isOnline === true ? (
-            <span className={css.online}>{isOnline}</span>
-          ) : (
-            <span className={css.ofline}>{isOnline}</span>
-          )}
-
-          <img className={'avatar'} src={avatar} alt="User avatar" width="48" />
-          <p className={'name'}>{name}</p>
-        </li>
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+          id={id}
+        />
       ))}
     </ListItem>
   );
